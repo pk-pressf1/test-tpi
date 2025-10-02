4a6c11ac12d90f1fbc1d5379775175e627f1dfa3
Evgeny Kylin <mail@pressf1.ru>
2025-10-02 18:51:15 +0500

    doc

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -44,7 +44,6 @@ return [
                  * Absolute paths to directory containing the swagger annotations are stored.
                  */
                 'annotations' => [
-                    base_path('app/Support'),
                     base_path('app/Http'),
                 ],
             ],
